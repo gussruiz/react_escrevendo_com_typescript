ce2ac5c6d47523c98eaa82ce4f8f33bbf65acdaf
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-05 22:18:28 -0300

    list, button and form components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Formulario from "./components/Formulario";
+import Lista from "./components/Lista";
 
 function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
+	return (
+		<div className="App">
+			<Formulario />
+			<Lista/>
+		</div>
+	);
 }
 
 export default App;
