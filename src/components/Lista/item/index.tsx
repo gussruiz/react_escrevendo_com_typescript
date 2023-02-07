3a6846027ae46675f040e11d2e7aedb52320c611
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-07 20:04:15 -0300

    interface, and the app is reactive LOL

--- src/components/Lista/item/index.tsx
+++ src/components/Lista/item/index.tsx
@@ -1,7 +1,8 @@
+import { ITarefa } from '../../../types/tarefa';
 import style from '../Lista.module.scss'
 
-export default function Item({tarefa, tempo}: {tarefa:string, tempo:string}){
-    return(
+export default function Item({ tarefa, tempo, completado, selecionado, id }: ITarefa) {
+    return (
         <li className={style.item}>
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,18 @@
 import Cronometro from "../components/Cronometro";
 import Formulario from "../components/Formulario";
 import Lista from "../components/Lista";
+import { useState } from 'react';
 import style from './App.module.scss';
+import { ITarefa } from "../types/tarefa";
 
 function App() {
+
+	const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+
 	return (
 		<div className={style.AppStyle}>
-			<Formulario />
-			<Lista/>
+			<Formulario setTarefas={setTarefas}/>
+			<Lista tarefas={tarefas}/>
 			<Cronometro/>
 		</div>
 	);
