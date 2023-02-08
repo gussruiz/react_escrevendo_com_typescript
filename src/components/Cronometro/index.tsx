da016e151fd5a450971ce08fe8092af6769c9f87
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-08 18:33:29 -0300

    final stuff and classes to functions

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,30 +1,19 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
-import { ITarefa } from "../../types/tarefa";
-import { useState } from "react";
-import { tempoParaSegundos } from "../../common/utils/time";
+import { tempoParaSegundos } from "../../common/utils/date";
 
-interface props{
-    selecionado: ITarefa | undefined
-}
-
-export default function Cronometro({...selecionado}: props){
-    const [tempo, setTempo] = useState<number>();
-    
-    if(selecionado.tempo){
-        setTempo(tempoParaSegundos(selecionado.tempo));
-    }
-
-
-
-    return(
-        <div className={style.cronometro}>
-            <p className={style.titulo} >Escolha um card e inicie o cronômetro</p>
-            <div className={style.relogioWrapper} >
-                <Relogio/>
-            </div>
-            <Botao texto="Começar"/>
-        </div>
-    );
+export default function Cronometro() {
+  console.log('conversao: ', tempoParaSegundos('01:01:01'))
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
+      <div className={style.relogioWrapper}>
+        <Relogio />
+      </div>
+      <Botao>
+        Começar!
+      </Botao>
+    </div>
+  )
 }
